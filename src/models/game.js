9ac0f987e86fd48159af2ef50e7a82ca9dbc04cd
Natasha Ewing <natasharai@me.com>
2016-12-19 10:48:39 -0800

    board is made working on the template to begin pop up modal functionality

--- src/models/game.js
+++ src/models/game.js
@@ -1,7 +1,8 @@
 import Backbone from 'backbone';
 import GameView from 'app/views/game_view';
+import $ from "jquery";
 
 const Game = Backbone.Model.extend({
   initialize: function() {
-    
+
   },
